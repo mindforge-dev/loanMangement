2043a9ea3bb0879bde9e9e172d535c5eba721607
thanhtike404 <thanhtike404web@gmail.com>
2026-02-07 08:41:53 +0630

    feat: Migrate from bcrypt to bcryptjs and add production server URL to OpenAPI documentation.

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,12 @@ export const app = express();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors(
+  {
+    origin: "*",
+    credentials: true,
+  }
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
